8765a74d2fe69fd4d636a7d67f1edcbc05974db1
adelehmg <90263397+adelehmg@users.noreply.github.com>
2021-10-08 00:14:10 +0800

    merged mohit + dl branch

--- FYP/FYP/DeepLearning/Script.py
+++ FYP/FYP/DeepLearning/Script.py
@@ -1,168 +1,211 @@
 
 #Just trying out some sample python code
 
-
-
-
 # #Imports
-# import sys
-# import cv2
-# import pathlib
-# import os
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import tensorflow as tf
-# from tensorflow.keras import layers
-# import keras
-# from glob import glob, iglob
-# from sklearn.metrics import confusion_matrix
-# import itertools
-# import pickle
-# from matplotlib.ticker import MaxNLocator
-# import operator
-# from decimal import Decimal
-# from sklearn.model_selection import train_test_split
-# import shutil
+import sys
+import cv2
+import pathlib
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+import keras
+from decimal import Decimal
+import shutil
+
+from tkinter import *
+from tkinter.ttk import *
+
+def test():
+    print("Reached python script")
 
 
-# #Global variable 
-# directory = "" 
+#Extract face and resize
+def extract_face(img_list, min_size = (200,200)):
+  idx = 0 
+  while idx < len(img_list):
+    img = img_list[idx]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors = 5, minSize= min_size ) #, minSize = (200,200)) 
+  
+    for (x, y, w, h) in faces:
+      #if x == 0 or y == 0 or w == 0 or h == 0:
+      #  print("None")
+      faces = img[y:y + h, x:x + w]
+      faces = cv2.resize(faces, (224,224),interpolation = cv2.INTER_AREA)
 
-
-# #Extract face and resize
-# def extract_face(img_list, min_size = (200,200)):
-#   idx = 0 
-#   while idx < len(img_list):
-#     img = img_list[idx]
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-#     faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors = 5, minSize= min_size ) #, minSize = (200,200)) 
-#    
-#     for (x, y, w, h) in faces:
-#       #if x == 0 or y == 0 or w == 0 or h == 0:
-#       #  print("None")
-#       faces = img[y:y + h, x:x + w]
-#       faces = cv2.resize(faces, (224,224),interpolation = cv2.INTER_AREA)
-# 
-#     if len(faces) != 0:
-#       img_list[idx] = faces
-#       idx += 1
-#     else:
-#       img_list.pop(idx)  
+    if len(faces) != 0:
+      img_list[idx] = faces
+      idx += 1
+    else:
+      img_list.pop(idx)
 
 
 # #Splitting video into frames, extracting and resizing
-# def extract_images(pathIn): #, pathOut):
-#     to_return = []
-#     count = 0
-#     vidcap = cv2.VideoCapture(pathIn)
-#     success,image = vidcap.read()    #Grabs, decodes and returns the next video frame.
-#     success = True
-#     while success:
-#         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*500))    # added this line. set() = set a property in video capture #0.167 for 6 frames per sec -> 6 img/3 sec
-#         #CAP_PROP_POS_MSEC = Current position of the video file in milliseconds.
-#         success,image = vidcap.read()
-#         if success == True:    #If have read in a new frame
-#           #cv2.imwrite(os.path.join(pathOut + "/frame_%d_.jpg" % count), image)     # save frame as JPEG file
-#           to_return.append(image)
-#         count = count + 1
+def slice_video(video_path):
+  sourceVideoDirectory = os.path.normpath(video_path)
 
-#     return to_return #pathOut
+  to_return = []
+  count = 0
+  vidcap = cv2.VideoCapture(sourceVideoDirectory)
+  success,image = vidcap.read()    #Grabs, decodes and returns the next video frame.
+  success = True
+  while success:
+      vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*500))    # added this line. set() = set a property in video capture #0.167 for 6 frames per sec -> 6 img/3 sec
+      #CAP_PROP_POS_MSEC = Current position of the video file in milliseconds.
+      success,image = vidcap.read()
+      if success == True:    #If have read in a new frame
+        #cv2.imwrite(os.path.join(pathOut + "/frame_%d_.jpg" % count), image)     # save frame as JPEG file
+        to_return.append(image)
+      count = count + 1
 
-# def slice_video(video_name):
-#   #directory must have been set, will be a global variable
-#   sourceVideoDirectory = os.path.join(directory, video_name)
-#   #targetVideoDirectory = os.path.join(directory + "sliced", video_name)
-
-#   #Create the target video directory if does not exist. Reference: https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python
-#   # pathlib.Path(targetVideoDirectory).mkdir(parents=True, exist_ok=True)
-
-#   #Split the video into frames. 
-#   return extract_images(sourceVideoDirectory) #, targetVideoDirectory)
+  return to_return
 
 
-# #Data duplication
-# def duplicate(img_list):
-#   n = len(img_list)
-#   if n % 6 != 0:
-#     duplicate_num = 6 - (n%6)
+#Data duplication
+def duplicate(img_list):
+  n = len(img_list)
+  if n % 6 != 0:
+    duplicate_num = 6 - (n%6)
 
-#     for i in range(duplicate_num):
-#       img_list.append(img_list[-1])
+    for i in range(duplicate_num):
+      img_list.append(img_list[-1])
 
-#   return len(img_list)
+  return len(img_list)
 
-# #Create list of images in np.arr form
-# def transform_img(img_list):
-#   #img_list.sort(key = lambda x : int(x[1]))
-#   img_list = np.asarray(img_list)
-#   img_list = [np.expand_dims(img,0) for img in img_list]
+#Create list of images in np.arr form
+def transform_img(img_list):
+  img_list = np.asarray(img_list)
+  img_list = [np.expand_dims(img,0) for img in img_list]
 
-#   return img_list #return a list of tuple (np.arr of image, image code)
+  return img_list #return a list of tuple (np.arr of image, image code)
 
 
-# #Predicted label and time 
-# def get_label_time(arr, threshold = 0.25):
-#   to_return = []
-#   time_str = "" 
+#Predicted label and time 
+def get_label_time(arr, threshold = 0.30):
+  label_code = {0: 'anger', 1: 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'sad', 5 : 'surprise'}
+  to_return = []
+  time_str = "" 
 
-#   for i in range(len(arr)):
-#     prob = arr[i][0]
-#     idx = arr[i][1][0]
+  for i in range(len(arr)):
+    prob = arr[i][0]
+    idx = arr[i][1][0]
 
-#     if prob > threshold:
-#       hour = i*3 // 3600
-#       minute = i*3 // 60
-#       second = i*3 % 60
+    if prob > threshold:
+      hour = i*3 // 3600
+      minute = i*3 // 60
+      second = i*3 % 60
 
-#       if hour == 0 and minute == 0 and second == 0:
-#         pass
-#       else:
-#         if hour != 0:
-#           time_str += str(hour)
+      if hour == 0 and minute == 0 and second == 0:
+        pass
+      else:
+        if hour != 0:
+          time_str += str(hour)
         
-#         # add minute
-#         if hour != 0 and len(str(minute)) == 1:
-#           time_str += ":0" + str(minute)
+        # add minute
+        if hour != 0 and len(str(minute)) == 1:
+          time_str += ":0" + str(minute)
         
-#         else: #hour == 0 and len(minute)
-#           time_str += str(minute)
+        else: #hour == 0 and len(minute)
+          time_str += str(minute)
 
-#         # add second
-#         if len(str(second)) == 1:
-#           time_str += ":0" + str(second)
+        # add second
+        if len(str(second)) == 1:
+          time_str += ":0" + str(second)
 
-#         else:
-#           time_str += ":" + str(second)
+        else:
+          time_str += ":" + str(second)
 
-#         to_return.append((label_code[idx], time_str))
-#       time_str = "" 
+        to_return.append((label_code[idx], time_str))
+      time_str = "" 
 
-#   return to_return
+  return to_return
 
-# #Main function
-# def main(file_name):
-#   # process file
-#   # 1 - Slice video
-#   extracted_img = slice_video(video_name)
+#Main function
+def main(directory):
+      
+  #Reference: https://www.youtube.com/watch?v=0WRMYdOwHYE&ab_channel=BroCode
+  #local function to display progress bar
+  def updateProgressWindow(percentValue, percentLabel, taskLabel):
+    window.update()
+    bar["value"] = percentValue
+    percent.set(percentLabel + "%")
+    text.set(taskLabel)
+    #window.update_idletasks()
 
-#   # 2 - Extract face only & resize image
-#   extract_face(extracted_img)
+  #create progress bar
+  window = Tk()
 
-#   # 3 - Do data duplication & tansfrom image
-#   duplicate(extracted_img)
-#   img_list = transform_img(extracted_img)
+  percent = StringVar()
+  text = StringVar()
+  bar = Progressbar(window, orient = HORIZONTAL, length= 300)
+  bar.pack(pady = 10) #Padding for y
+  percentLabel = Label(window, textvariable= percent).pack()
+  taskLabel = Label(window, textvariable= text).pack()
 
-#   # 5 - Do prediction
-#   predicted_list = []
-#   for i in range(0, len(img_list),6):
-#     input = img_list[i:i+6]
-#     yhat = trainedModel.predict(input) #Use verbose to get progress bar
-#     predicted_list.append((yhat.max(), np.argmax(yhat, axis=1)))
 
-#   return get_label_time(predicted_list)
+  #Initial value of progress bar
+  updateProgressWindow(0,"0","Started processing")
+
+
+  # process file
+  # 1 - Slice video
+  extracted_img = slice_video(directory)
+  updateProgressWindow(20,"20","Sliced the video into images")
+
+  # 2 - Extract face only & resize image
+  extract_face(extracted_img)
+  updateProgressWindow(40,"40","Extracted the face and resized")
+
+  # 3 - Do data duplication & tansfrom image
+  duplicate(extracted_img)
+  img_list = transform_img(extracted_img)
+  updateProgressWindow(60,"60","Duplicated and transformed data")
+
+  # 5 - Do prediction
+  #print(os.getcwd())
+  export_path = os.path.join(os.getcwd(), 'FYP', 'DeepLearning', 'Model.h5')
+  trainedModel = keras.models.load_model(export_path)
+  predicted_list = []
+  for i in range(0, len(img_list),6):
+    input = img_list[i:i+6]
+    yhat = trainedModel.predict(input) #Use verbose to get progress bar
+    predicted_list.append((yhat.max(), np.argmax(yhat, axis=1)))
+
+  updateProgressWindow(100,"100","Finished prediction")
+  window.after(5000, window.destroy)   #Automatically destroy the window after 5 seconds if not exited
+  window.mainloop()
+
+
+  return get_label_time(predicted_list)
+
+
+
+##################For brython update html elems
+# from browser import document, html
+
+# element = document.getElementById("test")
+
+# para = document.createElement("P")
+# text = document.createTextNode("Processing video progress")
+# para.appendChild(text)
+# element.appendChild(para)
+
+
+# progressBar = document.createElement("progress")
+# progressBar.setAttribute("id", "progressBarDL")
+# progressBar.setAttribute("value", "32")
+# progressBar.setAttribute("max", "100")
+
+# label = document.createElement("label")
+# label.setAttribute("for","progressBarDL")
+# label.setAttribute("innerHTML", "Deep Learning Processing progress:")   
+    
+# element.appendChild(progressBar)
+# element.appendChild(label)
+
+####################For brython update html elems
 
 
 
