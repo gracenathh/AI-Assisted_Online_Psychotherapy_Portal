8e33a1ffc4a22088aeec759de5659b1e43ec5d36
adelehmg <90263397+adelehmg@users.noreply.github.com>
2021-10-08 00:09:47 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,45 @@
+#to run: in cmd, navigate to /FYP, enter command: "set FLASK_APP=FYP:newapp" then enter "flask run"
+#to check db version in cmd, navigate to /FYP, enter command: "set FLASK_APP=FYP:newapp" then enter "flask db current --directory FYP/migrations"
+
+from flask import Flask
+from os.path import join, dirname, realpath
+import os
+from flask_sqlalchemy import SQLAlchemy, declarative_base
+from sqlalchemy.schema import MetaData
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+
+VIDEO_UPLOADER = join(dirname(realpath(__file__)), 'static/uploads/')
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'cc108b47c5a06f710fdb8bf12cac68e2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['UPLOAD_FOLDER'] = VIDEO_UPLOADER
+
+db = SQLAlchemy(app)
+migrate=Migrate(app,db, render_as_batch=True)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS']= True
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+"""
+meta = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+      })
+Base = declarative_base(metadata=meta)
+"""
+
+from FYP import routes
+
+newapp = app
---
+++ forms.py
@@ -0,0 +1,83 @@
+from flask.app import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields.core import DateTimeField, FormField, IntegerField, SelectField
+from wtforms.fields.simple import FileField, PasswordField, SubmitField
+from flask_wtf.file import FileAllowed
+from wtforms.form import Form
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from FYP.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    title = SelectField(u'Title', choices=[('Dr.'), ('Mr.'), ('Mrs.'), ('Ms.'), ('Mdm'), ('Miss')])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    organization = StringField('Organization')
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Register')
+
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email Already Registered!')
+    
+
+
+class PatientForm(FlaskForm):
+    gender = SelectField(u'Gender', choices=[('Male'), ('Female')])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    age = IntegerField('Age in Years')
+    country = StringField('Country')
+    #picture = FileField('Upload Picture', validators=[FileAllowed(['.jpg', 'png'])])
+
+    guardiantitle = SelectField(u'Title', choices=[('Dr.'), ('Mr.'), ('Mrs.'), ('Ms.'), ('Mdm'), ('Miss')])
+    guardianfirstname = StringField('First Name', validators=[DataRequired()])
+    guardianlastname = StringField('Last Name', validators=[DataRequired()])
+    relationship = SelectField(u'Relationship', choices=[('Parent'), ('Sibling'), ('Grandparent'), ('Guardian')])
+    email = StringField('Email', validators=[Email()])
+
+    submit = SubmitField('Add Record')
+
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Change Password')
+
+class UpdateDetailsForm(FlaskForm):
+    title = SelectField(u'Title', choices=[('Dr.'), ('Mr.'), ('Mrs.'), ('Ms.'), ('Mdm'), ('Miss')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    organization = StringField('Organization', validators=[DataRequired()])
+
+    submit = SubmitField('Update')
+
+class SearchPatientForm(FlaskForm):
+    search = StringField('Search')
+    
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('Email Does Not Exist. Please enter the email you have registered with.')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('New Password', validators=[DataRequired()])
+    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Reset Password')
