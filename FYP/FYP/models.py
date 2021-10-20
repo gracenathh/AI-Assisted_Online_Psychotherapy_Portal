c0b821f0b0517241b031f359b22f5586cd74fb18
adelehmg <90263397+adelehmg@users.noreply.github.com>
2021-10-21 04:23:10 +0800

    Update models.py

--- FYP/FYP/models.py
+++ FYP/FYP/models.py
@@ -1,3 +1,4 @@
+#models file details database classes that sqlalchemy will use to make tables for in which we will store data in. Each entry in the database is an object of one of these classes.
 from flask_login import LoginManager, login_manager, UserMixin, login_user, current_user
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -6,12 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref, column_property
 from FYP import db, login_manager, app
 
+#allows user to login and access their version of the web application by querying their ID from the database
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     __tablename__='user'
+    #primary key allows each user entry to automatically have a unique user ID
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120))
     title = db.Column(db.String(10))
@@ -19,13 +22,16 @@ class User(db.Model, UserMixin):
     lastname = db.Column(db.String(120))
     password = db.Column(db.String(60))
     organization = db.Column(db.String(60))
+    
+    #Links user back to its related patient records and videos uploaded. 
     patientRecord = db.relationship('Patient', backref='user', lazy='select')
     videoRecord = db.relationship('VideoFiles', backref='user', lazy='select')
 
+    #user get unique token string for use with reset password function
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps ({'user_id': self.id}).decode('utf-8')
-    
+    #check if token is valid when resetting password
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(app.config['SECRET_KEY'])
@@ -34,7 +40,8 @@ class User(db.Model, UserMixin):
         except:
             return None
         return User.query.get(user_id)
-
+    
+    #when user is queried from database, this information is printed out
     def __repr__(self):
         return f"User('{self.email}', '{self.title}', '{self.firstname}', '{self.lastname}', '{self.organization}')"
 
@@ -47,6 +54,7 @@ class Patient(db.Model):
     country = db.Column(db.String(60))
     age = db.Column(db.Integer)
     
+    #Links a patient record to its user and to any videos that specifies the patient
     therapyid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     patientvideos = db.relationship('VideoFiles', backref='patient', lazy='select')
 
@@ -75,7 +83,8 @@ class VideoFiles(db.Model):
 
     def __repr__(self):
         return f"VideoFiles('{self.videoName}')"
-
+    
+#for use with video upload to get metadata
 class Variables:
     username = getpass.getuser()
     date = datetime.now()
